d1794255f5919d561b470f113f2d97ef08a18436
cwhite204 <cwhite31@qub.ac.uk>
2017-08-26 15:31:22 +0100

    Fixed logout link navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :users, :controllers => { registrations: 'registrations' }
 
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
   root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
